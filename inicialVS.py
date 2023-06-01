55bed59a9eb4e073deb36d954242f43415eabf03
nicolas <nsantoscoronado@gmail.com>
2023-06-01 17:26:20 -0500

    cambio de nombre

---
+++ inicialVS.py
@@ -0,0 +1,12 @@
+
+"hola mundo de python en VScode"
+
+print("hola mundo!") 
+
+"Prueba para el VS code, texto ejemplo." 
+
+"Programa siempre tu código" 
+"como si el tipo que va a tener que mantenerlo en el futuro"
+"fuera un violento psicópata que sabe donde vives."
+
+
