55c5a49dedbb98d3847150b246e1e53fd8a41941
nicolas <nsantoscoronado@gmail.com>
2023-06-02 18:48:15 -0500

    nuevos cambios

--- inicialVS.py
+++ inicialVS.py
@@ -5,8 +5,13 @@ print("hola mundo!")
 
 "Prueba para el VS code, texto ejemplo." 
 
+my_variable = "texto"
+
 "Programa siempre tu código" 
 "como si el tipo que va a tener que mantenerlo en el futuro"
 "fuera un violento psicópata que sabe donde vives."
 
+"Desde aqui inicio la nueva rama (prueba)"
 
+"despues de aqui ire a prescionar branch para subir la rama 2 "
+"testeando nuevos comentarios para entender la creacion de nueva rama"
