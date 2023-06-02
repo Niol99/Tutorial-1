022d051dc3cfcb1827eba0ba326f6e9169b09980
nicolas <nsantoscoronado@gmail.com>
2023-06-02 18:42:55 -0500

    cambios nuevos

--- inicialVS.py
+++ inicialVS.py
@@ -5,8 +5,12 @@ print("hola mundo!")
 
 "Prueba para el VS code, texto ejemplo." 
 
+my_variable = "texto"
+
 "Programa siempre tu código" 
 "como si el tipo que va a tener que mantenerlo en el futuro"
 "fuera un violento psicópata que sabe donde vives."
 
+print(my_variable) 
 
+f specs. 
