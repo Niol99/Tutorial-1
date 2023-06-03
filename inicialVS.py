329d65a9a8984ad316560b4ae8fb27686965d467
nicolas <nsantoscoronado@gmail.com>
2023-06-02 23:39:45 -0500

    cambios

--- inicialVS.py
+++ inicialVS.py
@@ -1,7 +1,7 @@
 
 "hola mundo de python en VScode"
 
-print("hola mundo!") 
+
 
 "Prueba para el VS code, texto ejemplo." 
 
@@ -17,3 +17,17 @@ my_variable = "texto"
 "testeando nuevos comentarios para entender la creacion de nueva rama"
 
 "publicnando rama 2 " 
+
+my_variable = "hola" 
+my_variable2 = "soy nicolas" 
+
+"texto"
+
+
+my_string = "hola"
+my_string2 = "soy santos"
+
+"texto" 
+#comentario para depurar
+
+print(my_string2)
